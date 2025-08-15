76061346f22042d59b7993b11a1bd124273a089d
Billi0121 <gafarovbilol2009@gmail.com>
2025-08-15 05:50:40 -0500

    Finish

--- dynamic_parsing.py
+++ dynamic_parsing.py
@@ -1,9 +1,11 @@
-from requests_html import HTMLSession
 from bs4 import BeautifulSoup
+from requests_html import HTMLSession
+import time
 
 if __name__ == '__main__':
     session = HTMLSession()
     response = session.get('https://httpbin.org/')
+    response.html.render(sleep=3)
     soup = BeautifulSoup(response.html.html, 'lxml')
     swagger = soup.find(id='swagger-ui')
     print(swagger.prettify()) 
